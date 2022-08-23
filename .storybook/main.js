36934d47e5b86c4b39703b19a173eae06514c7d9
TristanLeclair <tristan.leclair-vani@alithya.com>
2022-08-22 20:25:09 -0400

    Update aliases and create atomic structure

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const toPath = (filePath) => path.join(process.cwd(), filePath);
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -9,5 +12,28 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@atoms': path.resolve(__dirname, '../src/components/atoms'),
+          '@molecules': path.resolve(__dirname, '../src/components/molecules'),
+          '@organisms': path.resolve(__dirname, '../src/components/organisms'),
+          '@templates': path.resolve(__dirname, '../src/components/templates'),
+          '@pages': path.resolve(__dirname, '../src/components/pages'),
+          '@components': path.resolve(__dirname, '../src/components'),
+          '@config': path.resolve(__dirname, '../src/config'),
+          '@utils': path.resolve(__dirname, '../src/utils'),
+          '@styles': path.resolve(__dirname, '../src/styles'),
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          'emotion-theming': toPath('node_modules/@emotion/react'),
+        },
+        modules: [...config.resolve.modules, toPath('src')],
+      },
+    };
   },
 };
